759e41f2873db4d0aa9c5a06b555c9818ef6e8b3
Thiago Seixas <ticolls@hotmail.com>
2023-08-01 16:38:47 -0300

    reseting project

--- api/puppeteer.config.cjs
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { join } = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-    cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
